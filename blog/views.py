17aaf2006688d13c802921269de13b4876ee2ed1
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-10-15 23:54:42 +0000

    Added a dummy data for posts in the views.py file

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+posts = [
+    {   
+    'author': 'John Doe',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'April 20, 2025'
+    },
+    {
+    'author': 'Jane Smith', 
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'April 21, 2025'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'blog/about.html')
